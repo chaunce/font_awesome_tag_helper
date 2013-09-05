f0dd1f4ce43cc6720ccd6f13b022c1c27ff02076
chaunce <chaunce.slc@gmail.com>
2013-09-05 12:38:57 -0600

    include tests with options

--- test/dummy/app/controllers/font_awesome_tag_helper_controller.rb
+++ test/dummy/app/controllers/font_awesome_tag_helper_controller.rb
@@ -1,5 +1,6 @@
 class FontAwesomeTagHelperController < ApplicationController
   def index
    @tag = params[:tag].to_sym
+   @options = params[:options] || {}
   end
 end
--- test/font_awesome_tag_helper_controller_test.rb
+++ test/font_awesome_tag_helper_controller_test.rb
@@ -2,12 +2,154 @@ require 'test_helper'
 require 'font_awesome_tag_helper_controller'
 
 class FontAwesomeTagHelperControllerTest < ActionController::TestCase
-  test "generate_tags" do
-    tag = 'search'
-    
+  test "generate_tag" do
+    tag = "search"
+
     get :index, { :tag => tag }
     assert_response 200
     assert_tag :tag => 'i', :attributes => { :class => "icon-#{tag}" }
+    assert_tag :tag => 'i', :children => { :count => 0 }
+    assert_tag :tag => 'i', :content => nil
+  end
+
+  test "generate_tag_without_options" do
+    tag = "search"
+
+    get :index, { :tag => tag }
+    assert_response 200
+    assert_tag :tag => 'i', :attributes => { :class => "icon-#{tag}" }
+    assert_tag :tag => 'i', :attributes => { :id => nil }
+    assert_tag :tag => 'i', :attributes => { :name => nil }
+    assert_tag :tag => 'i', :children => { :count => 0 }
+    assert_tag :tag => 'i', :content => nil
+  end
+
+  test "generate_tag_with_basic_options" do
+    tag = "search"
+    options = { id: "search-box", name: "search" }
+
+    get :index, { :tag => tag, :options => options }
+    assert_response 200
+    assert_tag :tag => 'i', :attributes => { :class => "icon-#{tag}" }
+    assert_tag :tag => 'i', :attributes => { :id => "search-box" }
+    assert_tag :tag => 'i', :attributes => { :name => "search" }
+    assert_tag :tag => 'i', :children => { :count => 0 }
+    assert_tag :tag => 'i', :content => nil
+  end
+
+  test "generate_tag_with_advanced_options" do
+    tag = "search"
+    options = { id: "search-box", name: "search", width: "fixed", size: "2x" }
+
+    get :index, { :tag => tag, :options => options }
+    assert_response 200
+    assert_tag :tag => 'i', :attributes => { :class => "icon-#{tag} icon-fixed-width icon-2x" }
+    assert_tag :tag => 'i', :attributes => { :id => "search-box" }
+    assert_tag :tag => 'i', :attributes => { :name => "search" }
+    assert_tag :tag => 'i', :children => { :count => 0 }
+    assert_tag :tag => 'i', :content => nil
+  end
+
+  test "generate_tag_with_boolean_option" do
+    tag = "search"
+    options = { id: "search-box", name: "search", width: "fixed", large: true }
+
+    get :index, { :tag => tag, :options => options }
+    assert_response 200
+    assert_tag :tag => 'i', :attributes => { :class => "icon-#{tag} icon-fixed-width icon-large" }
+    assert_tag :tag => 'i', :attributes => { :id => "search-box" }
+    assert_tag :tag => 'i', :attributes => { :name => "search" }
+    assert_tag :tag => 'i', :children => { :count => 0 }
+    assert_tag :tag => 'i', :content => nil
+  end
+
+  test "generate_tag_with_boolean_string_option" do
+    tag = "search"
+    options = { id: "search-box", name: "search", width: "fixed", large: "true" }
+
+    get :index, { :tag => tag, :options => options }
+    assert_response 200
+    assert_tag :tag => 'i', :attributes => { :class => "icon-#{tag} icon-fixed-width icon-large" }
+    assert_tag :tag => 'i', :attributes => { :id => "search-box" }
+    assert_tag :tag => 'i', :attributes => { :name => "search" }
+    assert_tag :tag => 'i', :children => { :count => 0 }
+    assert_tag :tag => 'i', :content => nil
+  end
+
+  test "generate_tag_with_false_boolean_option" do
+    tag = "search"
+    options = { id: "search-box", name: "search", width: "fixed", large: false }
+
+    get :index, { :tag => tag, :options => options }
+    assert_response 200
+    assert_tag :tag => 'i', :attributes => { :class => "icon-#{tag} icon-fixed-width" }
+    assert_tag :tag => 'i', :attributes => { :id => "search-box" }
+    assert_tag :tag => 'i', :attributes => { :name => "search" }
+    assert_tag :tag => 'i', :children => { :count => 0 }
+    assert_tag :tag => 'i', :content => nil
+  end
+
+  test "generate_tag_with_false_boolean_string_option" do
+    tag = "search"
+    options = { id: "search-box", name: "search", width: "fixed", large: "false" }
+
+    get :index, { :tag => tag, :options => options }
+    assert_response 200
+    assert_tag :tag => 'i', :attributes => { :class => "icon-#{tag} icon-fixed-width" }
+    assert_tag :tag => 'i', :attributes => { :id => "search-box" }
+    assert_tag :tag => 'i', :attributes => { :name => "search" }
+    assert_tag :tag => 'i', :children => { :count => 0 }
+    assert_tag :tag => 'i', :content => nil
+  end
+
+  test "generate_tag_with_many_advanced_options" do
+    tag = "search"
+    options = { id: "search-box", name: "search", size: "4x", large: false, border: "true", pull: "left", spin: true, li: "false", list: "li", width: "fixed-width", rotate: 180, flip: "none" }
+
+    get :index, { :tag => tag, :options => options }
+    assert_response 200
+    assert_tag :tag => 'i', :attributes => { :class => "icon-#{tag} icon-4x icon-border pull-left icon-spin icon-li icon-fixed-width icon-rotate-180" }
+    assert_tag :tag => 'i', :attributes => { :id => "search-box" }
+    assert_tag :tag => 'i', :attributes => { :name => "search" }
+    assert_tag :tag => 'i', :children => { :count => 0 }
+    assert_tag :tag => 'i', :content => nil
+  end
+
+  test "generate_tag_with_duplicate_options" do
+    tag = "search"
+    options = { id: "search-box", name: "search", size: "4x", large: true, li: "yes", list: "li", rotate: 180, flip: "horizontal" }
+
+    get :index, { :tag => tag, :options => options }
+    assert_response 200
+    assert_tag :tag => 'i', :attributes => { :class => "icon-#{tag} icon-4x icon-li icon-flip-horizontal" }
+    assert_tag :tag => 'i', :attributes => { :id => "search-box" }
+    assert_tag :tag => 'i', :attributes => { :name => "search" }
+    assert_tag :tag => 'i', :children => { :count => 0 }
+    assert_tag :tag => 'i', :content => nil
+  end
+  
+  test "generate_tag_with_invalid_options" do
+    tag = "search"
+    options = { id: "search-box", name: "search", popup: true, flash: "Yes!", hidden: "okay" }
+
+    get :index, { :tag => tag, :options => options }
+    assert_response 200
+    assert_tag :tag => 'i', :attributes => { :class => "icon-#{tag}" }
+    assert_tag :tag => 'i', :attributes => { :id => "search-box" }
+    assert_tag :tag => 'i', :attributes => { :name => "search" }
+    assert_tag :tag => 'i', :children => { :count => 0 }
+    assert_tag :tag => 'i', :content => nil
+  end
+  
+  test "generate_invalid_tag" do
+    tag = "search-invalid"
+    options = { id: "search-invalid-box", name: "search-invalid" }
+
+    get :index, { :tag => tag, :options => options }
+    assert_response 200
+    assert_tag :tag => 'i', :attributes => { :class => "icon-#{tag}" }
+    assert_tag :tag => 'i', :attributes => { :id => "search-invalid-box" }
+    assert_tag :tag => 'i', :attributes => { :name => "search-invalid" }
     assert_tag :tag => 'i', :children => { :count => 0 }
     assert_tag :tag => 'i', :content => nil
   end
