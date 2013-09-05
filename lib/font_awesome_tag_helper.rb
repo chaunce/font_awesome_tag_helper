5dacfce3f2ab52094eb03211d0bae7cc78eb93ad
chaunce <chaunce.slc@gmail.com>
2013-09-05 12:37:05 -0600

    :name option independent of :id

--- lib/font_awesome_tag_helper.rb
+++ lib/font_awesome_tag_helper.rb
@@ -2,8 +2,8 @@ module FontAwesomeTagHelper
   def icon_tag(icon_name, options = {})
     tag = "<i"
     tag += " id=\"#{options[:id].to_s}\"" if options[:id].present?
-    tag += " name=\"#{(options[:id] || options[:name]).to_s}\"" if options[:id].present? || options[:name].present?
     tag += " class=\"#{(["icon-#{icon_name}"] + options[:classes].to_a).join(' ')}\""
+    tag += " name=\"#{options[:name].to_s}\"" if options[:name].present?
     tag += "></i>"
     return tag.html_safe
   end
