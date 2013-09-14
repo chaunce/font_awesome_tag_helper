11c73c190a767b9f27587b9b6f400ac5693b2e6e
chaunce <chaunce.slc@gmail.com>
2013-09-13 23:36:54 -0600

    version 2.1

--- lib/font_awesome_tag_helper/version.rb
+++ lib/font_awesome_tag_helper/version.rb
@@ -2,8 +2,8 @@ module ExpiringAssetLinks
   # Contains information about this gem's version
   module Version
     MAJOR = 0
-    MINOR = 1
-    PATCH = 2
+    MINOR = 2
+    PATCH = 1
 
     # Returns a version string by joining <tt>MAJOR</tt>, <tt>MINOR</tt>, and <tt>PATCH</tt> with <tt>'.'</tt>
     #
